464fda8ec7491690aa5bdf8e71fa3952be5d9e5b
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-22 00:10:50 +0100

    Adding the Supplier Model

---
+++ Models/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockControlSystem.Models
+{
+    public class Supplier
+    {
+        public string Supcode { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        [Key, Column(Order = 0)]
+        public string Email { get; set; }
+        public string Contact { get; set; }
+
+        [Key, Column(Order = 0)]
+        public string PhoneNo { get; set; }
+        public int Sup_Last_Num { get; set; }
+        public DateTime Sup_Start_Date { get; set; }
+    }
+}
---
+++ SupplierRepository/ISupplierRepository.cs
@@ -0,0 +1,15 @@
+﻿using StockControlSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockControlSystem.SupplierRepository
+{
+    public interface ISupplierRepository
+    {
+        List<Supplier> GetSuppliers();
+
+
+    }
+}
