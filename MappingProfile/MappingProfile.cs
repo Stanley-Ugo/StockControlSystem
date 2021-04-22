b436f3c58eac004032d349b30d0784765d440289
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-23 00:44:21 +0100

    Created the Maps in the MappingProfile

--- MappingProfile/MappingProfile.cs
+++ MappingProfile/MappingProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using StockControlSystem.Models;
+using StockControlSystem.StockControlResources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StockControlSystem.MappingProfile
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Supplier, SupplierDTO>();
+
+            CreateMap<SupplierDTO, Supplier>();
+        }
     }
 }
