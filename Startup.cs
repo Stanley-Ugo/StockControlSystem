6a7759e0869cb7cffebb8c6bbcbad9efc0cac3bd
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-22 01:10:25 +0100

    Registering the service for dependency injection

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StockControlSystem.SupplierRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace StockControlSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<ISupplierRepository, MockSupplierData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- SupplierRepository/MockSupplierData.cs
+++ SupplierRepository/MockSupplierData.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using StockControlSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StockControlSystem.SupplierRepository
 {
-    public class MockSupplierData
+    public class MockSupplierData : ISupplierRepository
     {
+        public Supplier AddSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Supplier EditSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Supplier GetSupplier(string supcode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Supplier> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
