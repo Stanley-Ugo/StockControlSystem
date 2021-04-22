a40c9fadfdc685c7416017f5170f61fc08242b71
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-22 22:54:19 +0100

    Adding the StockControlContext class

---
+++ StockControlContext/StockControlContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockControlSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockControlSystem.StockControlContext
+{
+    public class StockControlContext : DbContext
+    {
+        public StockControlContext(DbContextOptions<StockControlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Supplier> St_Supplier { get; set; }
+    }
+}
