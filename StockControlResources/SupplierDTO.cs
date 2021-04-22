694100450402dca69f1e383780bcfb19feaaa879
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-23 00:39:20 +0100

    Adding a SupplierDTO Object Properties

--- StockControlResources/SupplierDTO.cs
+++ StockControlResources/SupplierDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,25 @@ namespace StockControlSystem.StockControlResources
 {
     public class SupplierDTO
     {
+        [StringLength(5, ErrorMessage = "Supply code can not be more than five characters!!!")]
+        [Required]
+        public string Supcode { get; set; }
+
+        [StringLength(50, ErrorMessage = "Invalid Name Property!!!")]
+        public string Name { get; set; }
+
+        [StringLength(50, ErrorMessage = "Invalid Address Property!!!")]
+        public string Address { get; set; }
+
+        [StringLength(30, ErrorMessage = "Invalid Email Property!!!")]
+        public string Email { get; set; }
+
+        [StringLength(20, ErrorMessage = "Invalid Contact Property!!!")]
+        public string Contact { get; set; }
+
+        [StringLength(30, ErrorMessage = "Invalid Contact Property!!!")]
+        public string PhoneNo { get; set; }
+        public int? Sup_Last_Num { get; set; }
+        public DateTime? Sup_Start_Date { get; set; }
     }
 }
