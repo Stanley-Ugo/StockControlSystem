f1774a57d50a3691a251f3675343f2724184e409
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-24 00:23:21 +0100

    Adding Unique key constraints

--- StockControlContext/StockControlDBContext.cs
+++ StockControlContext/StockControlDBContext.cs
@@ -17,5 +17,10 @@ namespace StockControlSystem.StockControlContext
         public DbSet<Supplier> St_Supplier { get; set; }
 
         public DbSet<St_Stkjournal> St_Stkjournals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<St_Stkjournal>().HasIndex(uniqueKey => uniqueKey.Stk_Coy).IsUnique();
+        }
     }
 }
