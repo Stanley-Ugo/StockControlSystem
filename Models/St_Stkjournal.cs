ee1eb1b64978a11ee258e4e43d35d6cdacd6b25a
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-24 00:13:54 +0100

    Adding the Properties

--- Models/St_Stkjournal.cs
+++ Models/St_Stkjournal.cs
@@ -7,6 +7,17 @@ namespace StockControlSystem.Models
 {
     public class St_Stkjournal
     {
-
-    }
+		public string stk_coy { get; set; }
+		public string stk_branch { get; set; }
+		public string stk_year { get; set; }
+		public string stk_month { get; set; }
+		public string stk_loc { get; set; }
+		public string stk_type { get; set; }
+		public string stk_account { get; set; }
+		public decimal stk_debit { get; set; }
+		public decimal stk_credit { get; set; }
+		public string stk_rem { get; set; }
+		public string stk_update { get; set; }
+		public string stk_period { get; set; }
+	}
 }
