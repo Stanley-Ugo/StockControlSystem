33c94aee0e1f6686ab9433f9ba8c147755084d9f
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-24 00:43:50 +0100

    building Application

--- Models/St_Stkjournal.cs
+++ Models/St_Stkjournal.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StockControlSystem.Models
 {
+	[Table("St_Stkjournal")]
     public class St_Stkjournal
     {
 		public string Stk_Coy { get; set; }
