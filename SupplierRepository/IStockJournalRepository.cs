4baa61b888464c5698ef7fe342986423ce385710
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-25 03:23:52 +0100

    Adding the methods of the repository Interface

--- SupplierRepository/IStockJournalRepository.cs
+++ SupplierRepository/IStockJournalRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockControlSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,14 @@ namespace StockControlSystem.SupplierRepository
 {
     public interface IStockJournalRepository
     {
+        List<St_Stkjournal> GetStockJournals();
 
+        St_Stkjournal GetStockJournal(string company);
+
+        St_Stkjournal AddStockJournal(St_Stkjournal stkJournal);
+
+        void DeleteStockJournal(St_Stkjournal stkJournal);
+
+        St_Stkjournal EditStockJournal(St_Stkjournal stkJournal);
     }
 }
