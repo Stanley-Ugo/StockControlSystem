2df98ae722e6f14ec884ae91618d960bea02325f
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-25 03:13:16 +0100

    Adding a few Controller Attributes

--- Controllers/StockJournalController.cs
+++ Controllers/StockJournalController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace StockControlSystem.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class StockJournalController : Controller
     {
 
