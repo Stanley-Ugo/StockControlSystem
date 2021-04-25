a5664a5e60b87033bb1e773f4981c23c27385547
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-26 00:14:45 +0100

    Adding the IStockJournalRepository Interface

--- SupplierRepository/SqlStockJournalData.cs
+++ SupplierRepository/SqlStockJournalData.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using StockControlSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StockControlSystem.SupplierRepository
 {
-    public class SqlStockJournalData
+    public class SqlStockJournalData : IStockJournalRepository
     {
+        public St_Stkjournal AddStockJournal(St_Stkjournal stkJournal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStockJournal(St_Stkjournal stkJournal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public St_Stkjournal EditStockJournal(St_Stkjournal stkJournal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public St_Stkjournal GetStockJournal(string company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<St_Stkjournal> GetStockJournals()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
