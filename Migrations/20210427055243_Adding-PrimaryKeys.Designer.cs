ad121772ff652e910ae2c7b935898123450fd0a2
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-27 06:53:31 +0100

    adding a new migration for primaryKeys

--- Migrations/20210427055243_Adding-PrimaryKeys.Designer.cs
+++ Migrations/20210427055243_Adding-PrimaryKeys.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockControlSystem.StockControlContext;
 
 namespace StockControlSystem.Migrations
 {
     [DbContext(typeof(StockControlDBContext))]
-    partial class StockControlDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210427055243_Adding-PrimaryKeys")]
+    partial class AddingPrimaryKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210427055243_Adding-PrimaryKeys.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockControlSystem.Migrations
+{
+    public partial class AddingPrimaryKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Account",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Branch",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Coy",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Loc",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Month",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Type",
+                table: "St_Stkjournal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_St_Stkjournal_Stk_Year",
+                table: "St_Stkjournal");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Year",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Type",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Month",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Loc",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Coy",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Branch",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Account",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_St_Stkjournal",
+                table: "St_Stkjournal",
+                columns: new[] { "Stk_Coy", "Stk_Branch", "Stk_Year", "Stk_Month", "Stk_Loc", "Stk_Type", "Stk_Account" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_St_Stkjournal",
+                table: "St_Stkjournal");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Account",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Type",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Loc",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Month",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Year",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Branch",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Stk_Coy",
+                table: "St_Stkjournal",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Account",
+                table: "St_Stkjournal",
+                column: "Stk_Account",
+                unique: true,
+                filter: "[Stk_Account] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Branch",
+                table: "St_Stkjournal",
+                column: "Stk_Branch",
+                unique: true,
+                filter: "[Stk_Branch] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Coy",
+                table: "St_Stkjournal",
+                column: "Stk_Coy",
+                unique: true,
+                filter: "[Stk_Coy] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Loc",
+                table: "St_Stkjournal",
+                column: "Stk_Loc",
+                unique: true,
+                filter: "[Stk_Loc] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Month",
+                table: "St_Stkjournal",
+                column: "Stk_Month",
+                unique: true,
+                filter: "[Stk_Month] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Type",
+                table: "St_Stkjournal",
+                column: "Stk_Type",
+                unique: true,
+                filter: "[Stk_Type] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_St_Stkjournal_Stk_Year",
+                table: "St_Stkjournal",
+                column: "Stk_Year",
+                unique: true,
+                filter: "[Stk_Year] IS NOT NULL");
+        }
+    }
+}
